5620bacc34ecda08e4e916129de2a8a2cde4ee3f
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 20:43:35 -0500

    Update Professor.java

--- src/main/java/com/UMB/taller/domain/Professor.java
+++ src/main/java/com/UMB/taller/domain/Professor.java
@@ -1,1 +1,32 @@
+package com.umb.taller.domain;
 
+public class Professor extends Person {
+
+    private String employeeCode;
+    private String department;
+
+    public Professor(
+            String id,
+            String name,
+            String email,
+            String employeeCode,
+            String department) {
+
+        super(id, name, email);
+        this.employeeCode = employeeCode;
+        this.department = department;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String getRole() {
+        return "Professor";
+    }
+}
