9eedfcae465cd0231e8f38ab041ce3086a39dac3
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:42:52 -0500

    Update UserService.java

--- src/main/java/com/umb/taller/application/UserService.java
+++ src/main/java/com/umb/taller/application/UserService.java
@@ -1,1 +1,15 @@
+package com.umb.taller.application;
 
+public class UserService {
+
+    public void createUser(String username) {
+
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Username cannot be empty"
+            );
+        }
+
+        System.out.println("User created: " + username);
+    }
+}
