0e7aa4a5429e8b0a46fdbd6446b2e663b0406b4d
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:46:28 -0500

    Define generic Repository interface with CRUD methods

--- src/main/java/com/umb/taller/domain/repository/Repository.java
+++ src/main/java/com/umb/taller/domain/repository/Repository.java
@@ -1,1 +1,15 @@
+package com.umb.taller.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T, ID> {
+
+    T save(T entity);
+
+    Optional<T> findById(ID id);
+
+    List<T> findAll();
+
+    void deleteById(ID id);
+}
