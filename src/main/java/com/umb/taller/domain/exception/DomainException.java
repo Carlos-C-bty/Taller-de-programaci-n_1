c1e679dfddac69187afd6207b11f2d94e74a142c
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:44:44 -0500

    Update DomainException.java

--- src/main/java/com/umb/taller/domain/exception/DomainException.java
+++ src/main/java/com/umb/taller/domain/exception/DomainException.java
@@ -1,1 +1,16 @@
+package com.umb.taller.domain.exception;
 
+public abstract class DomainException
+        extends AppException {
+
+    protected DomainException(String message) {
+        super(message);
+    }
+
+    protected DomainException(
+            String message,
+            Throwable cause) {
+
+        super(message, cause);
+    }
+}
