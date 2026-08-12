18b604064d495496c67dfa3556edecb838f54219
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 20:43:06 -0500

    Update and rename Course.java to Estudent.java

---
+++ src/main/java/com/UMB/taller/domain/Estudent.java
@@ -0,0 +1,32 @@
+package com.umb.taller.domain;
+
+public class Student extends Person {
+
+    private String studentCode;
+    private String program;
+
+    public Student(
+            String id,
+            String name,
+            String email,
+            String studentCode,
+            String program) {
+
+        super(id, name, email);
+        this.studentCode = studentCode;
+        this.program = program;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    @Override
+    public String getRole() {
+        return "Student";
+    }
+}
