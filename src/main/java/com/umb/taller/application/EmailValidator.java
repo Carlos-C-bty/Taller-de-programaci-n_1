2bb9306097b49d9c11ef9f2fbcb27e641fa9e2bc
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:43:51 -0500

    Update EmailValidator.java

--- src/main/java/com/umb/taller/application/EmailValidator.java
+++ src/main/java/com/umb/taller/application/EmailValidator.java
@@ -1,1 +1,15 @@
+package com.umb.taller.application;
 
+public class EmailValidator {
+
+    public boolean isValid(String email) {
+        return isValidStatic(email);
+    }
+
+    public static boolean isValidStatic(String email) {
+
+        return email != null
+                && email.contains("@")
+                && email.contains(".");
+    }
+}
