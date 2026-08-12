a5f3564059b0026275b5635ebf2b3fc1854d95e7
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:43:35 -0500

    Update ReportService.java

--- src/main/java/com/umb/taller/application/ReportService.java
+++ src/main/java/com/umb/taller/application/ReportService.java
@@ -1,1 +1,11 @@
+package com.umb.taller.application;
 
+public class ReportService {
+
+    public void generateReport() {
+
+        System.out.println(
+                "Report generated successfully."
+        );
+    }
+}
