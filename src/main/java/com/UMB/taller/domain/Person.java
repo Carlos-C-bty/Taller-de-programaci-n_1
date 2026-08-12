30ec6ab3d3f297a23cb583ec7318ba2b9e1e9b96
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 20:42:21 -0500

    Update Person.java

--- src/main/java/com/UMB/taller/domain/Person.java
+++ src/main/java/com/UMB/taller/domain/Person.java
@@ -1,1 +1,31 @@
+package com.umb.taller.domain;
+public abstract class Person {
 
+    private String id;
+    private String name;
+    private String email;
+
+    public Person(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public abstract String getRole();
+}
