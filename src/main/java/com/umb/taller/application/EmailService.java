d5895fb8284d3cad59a583a0c816d0c991817fbd
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:43:09 -0500

    Update EmailService.java

--- src/main/java/com/umb/taller/application/EmailService.java
+++ src/main/java/com/umb/taller/application/EmailService.java
@@ -1,1 +1,16 @@
+package com.umb.taller.application;
 
+public class EmailService {
+
+    public void sendEmail(String email, String message) {
+
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Email cannot be empty"
+            );
+        }
+
+        System.out.println("Email sent to: " + email);
+        System.out.println("Message: " + message);
+    }
+}
