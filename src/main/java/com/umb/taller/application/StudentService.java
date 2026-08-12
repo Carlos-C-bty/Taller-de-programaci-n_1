4ed528e52a98ff8391368cc89d4190c5b2779409
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:50:20 -0500

    Update StudentService.java

--- src/main/java/com/umb/taller/application/StudentService.java
+++ src/main/java/com/umb/taller/application/StudentService.java
@@ -1,1 +1,54 @@
+package com.umb.taller.application;
 
+import com.umb.taller.domain.Student;
+import com.umb.taller.domain.exception.EntityNotFoundException;
+import com.umb.taller.domain.exception.ValidationException;
+import com.umb.taller.domain.repository.Repository;
+
+public class StudentService {
+
+    private final Repository<Student, String>
+            studentRepository;
+
+    public StudentService(
+            Repository<Student, String> studentRepository) {
+
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+
+        if (student == null) {
+
+            throw new ValidationException(
+                    "Student cannot be null"
+            );
+        }
+
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(String id) {
+
+        return studentRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException(
+                                "Student not found: " + id
+                        ));
+    }
+
+    public void deleteStudent(String id) {
+
+        if (studentRepository
+                .findById(id)
+                .isEmpty()) {
+
+            throw new EntityNotFoundException(
+                    "Student not found: " + id
+            );
+        }
+
+        studentRepository.deleteById(id);
+    }
+}
