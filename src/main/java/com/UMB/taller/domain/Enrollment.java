025963c9c7266c259391de5592a661b724ed77db
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 20:43:54 -0500

    Update Enrollment.java

--- src/main/java/com/UMB/taller/domain/Enrollment.java
+++ src/main/java/com/UMB/taller/domain/Enrollment.java
@@ -1,1 +1,40 @@
+package com.umb.taller.domain;
 
+public class Enrollment {
+
+    private Student student;
+    private String courseName;
+    private double grade;
+
+    public Enrollment(Student student, String courseName) {
+        this.student = student;
+        this.courseName = courseName;
+        this.grade = 0.0;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        if (grade < 0 || grade > 5) {
+            throw new IllegalArgumentException(
+                    "Grade must be between 0 and 5"
+            );
+        }
+
+        this.grade = grade;
+    }
+
+    public boolean hasPassed() {
+        return grade >= 3.0;
+    }
+}
