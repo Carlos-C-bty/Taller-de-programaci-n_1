47128c99f53814c69510a34dc136f70df58ff8f3
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:50:39 -0500

    Update EnrollmentService.java

--- src/main/java/com/umb/taller/application/EnrollmentService.java
+++ src/main/java/com/umb/taller/application/EnrollmentService.java
@@ -1,1 +1,27 @@
+package com.umb.taller.application;
 
+import com.umb.taller.domain.Student;
+import com.umb.taller.domain.exception.EntityNotFoundException;
+import com.umb.taller.domain.repository.Repository;
+
+public class EnrollmentService {
+
+    private final Repository<Student, String>
+            studentRepository;
+
+    public EnrollmentService(
+            Repository<Student, String> studentRepository) {
+
+        this.studentRepository = studentRepository;
+    }
+
+    public Student findStudent(String id) {
+
+        return studentRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException(
+                                "Student not found: " + id
+                        ));
+    }
+}
