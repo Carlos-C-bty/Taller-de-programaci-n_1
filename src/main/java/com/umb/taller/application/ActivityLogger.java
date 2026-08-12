031844aa50154ed9de7f1d1c2be1a636c612c49f
carlos cuevas <carloscuevas.eb@academia.umb.edu.co>
2026-08-11 21:44:10 -0500

    Update ActivityLogger.java

--- src/main/java/com/umb/taller/application/ActivityLogger.java
+++ src/main/java/com/umb/taller/application/ActivityLogger.java
@@ -1,1 +1,15 @@
+package com.umb.taller.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ActivityLogger {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(ActivityLogger.class);
+
+    public void logActivity(String activity) {
+
+        logger.info("Activity: {}", activity);
+    }
+}
